edfbce9e02424c824c8723875266d6d3ad50e9ca
Mihir Gadgil <Mihir.Gadgil@lntinfotech.com>
2020-02-03 19:12:43 +0530

    starting files

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://mihir_rw:sZrel446DvwrY8Fm@cluster0-owaic.mongodb.net/test?retryWrites=true", { useNewUrlParser: true })
+.then(() => {
+  console.log('connection established');
+})
+.catch((err) => {
+  console.log('connection failed '+err.stack);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
